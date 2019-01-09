01f013b8775b558a91d0816520719760863266b4
akshanshverma <akshansh.verma@outlook.com>
2019-01-09 11:33:42 +0530

    testing

---
+++ resources/js/components/BranchTest.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+
+export default class BranchTest extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        console.log(this.state);
+
+        return (
+            <div>
+               hi
+            </div>
+        )
+    }
+}
